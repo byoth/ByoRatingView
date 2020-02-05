a03fbaee10f9fcb67f82d7ccf85b0e3e413c8e23
Byoth <taehyunjo97@gmail.com>
2020-02-05 16:41:34 +0900

    Initial commit

---
+++ ByoRatingView/ByoRatingView.h
@@ -0,0 +1,19 @@
+//
+//  ByoRatingView.h
+//  ByoRatingView
+//
+//  Created by Taehyun Jo on 2020/02/05.
+//  Copyright © 2020 byo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ByoRatingView.
+FOUNDATION_EXPORT double ByoRatingViewVersionNumber;
+
+//! Project version string for ByoRatingView.
+FOUNDATION_EXPORT const unsigned char ByoRatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ByoRatingView/PublicHeader.h>
+
+
